4d058b9f28784c93ec7338dbdcd34852fd15cd79
cjw20 <54556219+cjw20@users.noreply.github.com>
2020-02-11 13:12:41 -0500

    Added ui that tracks score

--- Roll-A-Ball/Assets/Scripts/PlayerController.cs
+++ Roll-A-Ball/Assets/Scripts/PlayerController.cs
@@ -1,16 +1,26 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+
 
 public class PlayerController : MonoBehaviour
 {
     // Start is called before the first frame update
 
     public float speed;
+    public Text countText;
+    public Text winText;
+
+
     private Rigidbody rb;
+    private int count;
     void Start()
     {
         rb = GetComponent<Rigidbody>();
+        count = 0;
+        SetCountText();
+        winText.text = "";
     }
 
     // Update is called once per frame
@@ -31,6 +41,16 @@ public class PlayerController : MonoBehaviour
         if (other.gameObject.CompareTag("Pickup"))
         {
             other.gameObject.SetActive(false);
+            count++;
+            SetCountText();
+        }
+    }
+    void SetCountText()
+    {
+        countText.text = "Count: " + count.ToString();
+        if (count >= 10)
+        {
+            winText.text = "You Win!";
         }
     }
 }
